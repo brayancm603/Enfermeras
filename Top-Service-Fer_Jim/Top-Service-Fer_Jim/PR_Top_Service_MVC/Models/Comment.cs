a5dc4e208ec35373dbe6d3ffaf372abba87170fd
Braian Canelas <cmb0030178@est.univalle.edu>
2023-09-29 23:19:59 -0400

    pruebas

---
+++ Top-Service-Fer_Jim/Top-Service-Fer_Jim/PR_Top_Service_MVC/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PR_Top_Service_MVC.Models
+{
+    public partial class Comment
+    {
+        [Key]
+        public int IdComment { get; set; }
+        public string Description { get; set; }
+        public int IdProfessional { get; set; }
+
+        public virtual Profesional? IdProfessionalNavigation { get; set; } = null!;
+    }
+}
