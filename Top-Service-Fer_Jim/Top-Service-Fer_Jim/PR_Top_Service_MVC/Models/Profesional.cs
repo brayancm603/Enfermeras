e233cafe587cd568d4c8810034c2da45d3b9fdd8
Braian Canelas <cmb0030178@est.univalle.edu>
2023-10-09 23:29:26 -0400

    Mejora de Frondtend de Home

--- Top-Service-Fer_Jim/Top-Service-Fer_Jim/PR_Top_Service_MVC/Models/Profesional.cs
+++ Top-Service-Fer_Jim/Top-Service-Fer_Jim/PR_Top_Service_MVC/Models/Profesional.cs
@@ -20,7 +20,6 @@ namespace PR_Top_Service_MVC.Models
         public DateTime Birthdate { get; set; }
         public double? Rating { get; set; }
         public virtual Person? IdProfesionalNavigation { get; set; } = null!;
-        public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
         public virtual ICollection<Postulation>? Postulations { get; set; }
         public virtual ICollection<Service>? Services { get; set; }
         public virtual ICollection<Quotation> Quotations { get; set; }
